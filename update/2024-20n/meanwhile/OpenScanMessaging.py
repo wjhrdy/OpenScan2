7ccabd18b60dfa5009cdeb59ef742ccd02d5aa7e
Stealth <stealth@MacBook-Pro-de-Stealth.local>
2024-10-13 20:33:53 +0200

    Messaging extracted to external library

--- update/2024-20n/meanwhile/OpenScanMessaging.py
+++ update/2024-20n/meanwhile/OpenScanMessaging.py
@@ -3,44 +3,31 @@ from OpenScanCommon import load_str, load_bool
 import telegram
 import asyncio
 
-
 class Messaging:
     def __init__(self) -> None:
-        self.telegram_enable: bool = load_bool('telegram_enable')
-        if self.telegram_enable:
+        self.telegram_enabled: bool = load_bool('telegram_enabled')
+        print(f"Telegram enabled: {self.telegram_enabled}")
+        if self.telegram_enabled:
             self.telegram_api_token: str = load_str('telegram_api_token')
             self.telegram_client_id: str = load_str('telegram_client_id')
 
-    async def send(self, message: str, chat_id: str, token: str):
+    async def send(self, message: str):
         """
-        Send a message "msg" to a telegram user or group specified by "chat_id"
-        msg         [str]: Text of the message to be sent. Max 4096 characters after entities parsing.
-        chat_id [int/str]: Unique identifier for the target chat or username of the target channel (in the format @channelusername)
-        token       [str]: Bot's unique authentication token.
+        Send a message using the Telegram API.
         """
-        chat_id = self.telegram_client_id
-        token = self.telegram_client_id
-        bot = telegram.Bot(token=token)
+        if not self.telegram_enabled:
+            print("Telegram is not enabled.")
+            return
+
+        bot = telegram.Bot(token=self.telegram_api_token)
         try:
-            await bot.sendMessage(chat_id=chat_id, text=message)
-            
+            await bot.send_message(chat_id=self.telegram_client_id, text=message)
+            print('Message Sent!')
         except telegram.error.TelegramError as e:
             print(f"Failed to send message: {e}")
-        print('Message Sent!')
 
-    def send_sessage(self, message):
+    def send_message(self, message: str):
         """
-        This method is used to send a message using the Telegram API. It takes a message, chat_id, and token as parameters.
-        The message is the text of the message to be sent, the chat_id is the unique identifier for the target chat or username of the target channel, and the token is the bot's unique authentication token.
-        The method first checks if the Telegram API is enabled. If it is, it creates a new instance of the Telegram bot using the token.
-        It then tries to send the message using the bot. If an error occurs, it prints a message to the console.
-        Finally, it prints a message to the console indicating that the message has been sent.
+        Wrapper method to call the asynchronous send method.
         """
-        asyncio.run(
-            self.send(
-                message=message,
-                chat_id=self.telegram_client_id,
-                token=self.telegram_api_token
-            )
-        )
-
+        asyncio.run(self.send(message))
